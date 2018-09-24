87ce41fedd945186f2c7913f8bf6f163b6554d93
dharmen.s <dharmen.s@milestoneinternet.info>
2018-09-24 19:03:29 +0530

    we can say, partially done...

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ let selectedChipBtns;
 let helpLabel = document.getElementById('helpLabel');
 let selectedChipContainer = document.getElementById('selectedChipContainer');
 let chipCotainer = document.getElementById('chipCotainer');
-let widthOfLastMovedChip = 0;
+let rightOfLastMovedChip = 0;
+let count = 0;
 
 function initChipBtns() {
     chipBtns = document.getElementsByClassName('chip-btn');
@@ -14,14 +15,13 @@ function initChipBtns() {
         $(chipBtns[i]).css('top', pos.top);
     }
 }
-initChipBtns();
 
 function initSelectedChipBtns() {
     selectedChipBtns = document.getElementsByClassName('selected-chip-btn');
     for (let i = 0; i < selectedChipBtns.length; i++) {
-        selectedChipBtns[i].addEventListener("click", function () {
-            removeFromSelectedItem(selectedChipBtns[i])
-        });
+        let pos = $(selectedChipBtns[i]).position();
+        $(selectedChipBtns[i]).css('left', pos.left);
+        $(selectedChipBtns[i]).css('top', pos.top);
     }
 }
 
@@ -29,41 +29,59 @@ function addToSelectedItem(ele) {
     let pos = $(ele).position();
     $(ele).css('left', pos.left);
     $(ele).css('top', pos.top);
-    $(ele).removeClass('chip-btn');
+    if ($(ele).hasClass('chip-btn')) {
+        count++;
+        $(ele).removeClass('chip-btn');
 
-    initChipBtns();
-    for (let i = 0; i < chipBtns.length; i++) {
-        let left = $(chipBtns[i]).css('left');
-        left = left.substring(0, left.length - 2);
-        let top = $(chipBtns[i]).css('top');
-        top = top.substring(0, top.length - 2);
-        let chiPos = {
-            left: +left,
-            top: +top
+        initChipBtns();
+        for (let i = 0; i < chipBtns.length; i++) {
+            let left = $(chipBtns[i]).css('left');
+            left = left.substring(0, left.length - 2);
+            let top = $(chipBtns[i]).css('top');
+            top = top.substring(0, top.length - 2);
+            let chiPos = {
+                left: +left,
+                top: +top
+            }
+            $(chipBtns[i]).addClass('position-absolute');
+            if (chiPos.left > pos.left && chiPos.top === pos.top) {
+                $(chipBtns[i]).animate({
+                    left: '-=' + ($(ele).width() + 30) + 'px'
+                }, 500);
+            }
         }
-        $(chipBtns[i]).addClass('position-absolute');
-        if (chiPos.left > pos.left && chiPos.top === pos.top) {
-            $(chipBtns[i]).animate({
-                left: '-=' + ($(ele).width() + 30) + 'px'
-            }, 500);
+        rightOfLastMovedChip += $(ele).width() + 30;
+        initSelectedChipBtns();
+        for (let j = 0; j < selectedChipBtns.length; j++) {
+            if (selectedChipBtns.length < 4 || j !== 0) {
+                if (j === count - 4 && j !== 0) {
+                    $(selectedChipBtns[count - 4]).addClass('merging-chip');
+                }
+                else {
+                    $(selectedChipBtns[j]).animate({
+                        left: '+=' + ($(ele).width() + 30) + 'px'
+                    }, 500);
+                }
+            }
         }
-        // else if (chiPos.top > pos.top && chiPos.left < pos.left) {
-        //     $(chipBtns[i]).animate({
-        //         top: '-=' + ($(ele).height()) + 'px',
-        //         right: '+=' + ($(ele).width() + 30) + 'px'
-        //     }, 500);
-        // }
-    }
+        if (rightOfLastMovedChip > 301) {
+            if (rightOfLastMovedChip > 301.5) {
+                $(selectedChipBtns[0]).addClass('counter-chip');
+            }
+            setTimeout(function () {
+                $(selectedChipBtns[0]).removeClass('counter-chip');
+            }, 300);
+            setTimeout(function () {
+                selectedChipBtns[0].innerHTML = '+' + (count - 3);
+            }, 100);
+        }
+        $(ele).addClass('position-absolute');
+        $(ele).addClass('selected-chip-btn');
+        $(ele).animate({
+            top: '20px',
+            left: 12 + 'px'
+        }, 500);
 
-    $(ele).addClass('position-absolute');
-    $(ele).addClass('selected-chip-btn');
-    $(ele).animate({
-        top: '20px',
-        left: (widthOfLastMovedChip + 12) + 'px'
-    }, 500);
-    widthOfLastMovedChip += $(ele).width() + 30;
-    let value = ele.getAttribute('value');
-    if (selectedItems.length === 0) {
         helpLabel.innerHTML = '&nbsp;';
     }
 }
